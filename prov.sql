78db5fcc0d1a0cfe97faa69ae86a3b9d7d530fdd
Julia Maciel <143205314+juliamacielm@users.noreply.github.com>
2024-05-14 18:17:50 -0300

    Add files via upload

---
+++ prov.sql
@@ -0,0 +1,71 @@
+use avaliacao_22c;
+
+create database nsk
+
+insert table livro
+titulo varchar (255);
+ano_publicacao int;
+autor varchar(255),
+editora varchar(255),
+
+
+
+
+
+
+insert into livros (titulo, autor, ano_publicacao, categoria, editora, num_paginas, idioma)
+values "(As cronicas de narnia", "1950", "C.S. Lewis", "harper collins", "768", "frances", "fantasia")
+
+
+
+update livros
+int editora = "Plume Books"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
